2f26318ba5cb4aaba21ef48f147c50417bafb8df
eldergsilva <eldergsilva@gmail.com>
2026-04-08 13:44:22 -0300

    exercicio 02

---
+++ 02/index.js
@@ -0,0 +1,15 @@
+const palavras = ["livro", "caneta", "sol", "carro", "orelha"];
+ 
+
+function verificarPalavras (palavras) {
+    for(let palavra of palavras){
+        if(palavra.length ){
+            console.log(`existe palavra inválida`);
+            break;
+        } 
+               
+                
+    }
+}
+
+verificarPalavras (palavras)
